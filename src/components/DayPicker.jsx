29dc675d3f3d1bb1b7e4a992c329cf28394cbf52
Bahare Goudarzi <goudarzi.b@yahoo.com>
2025-10-25 20:04:22 +0330

    fix: changed the look of the calendar

--- src/components/DayPicker.jsx
+++ src/components/DayPicker.jsx
@@ -1,13 +1,15 @@
 function dayPicker({day, getDay}) {
 
   return(
-    <input 
-      type='date' 
-      min="2024-01-01" 
-      max="2024-01-09"
-      value={day}
-      onChange={getDay}
-      className="bg-[#1b1b1b] text-[#1b1b1b]" />
+    <div className="rounded-full p-2 bg-[#292929]">
+      <input 
+        type='date' 
+        min="2024-01-01" 
+        max="2024-01-09"
+        value={day}
+        onChange={getDay}
+        className="bg-[#292929] text-[#538e6a]" />
+    </div>
   )
 };
 
